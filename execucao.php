9e3ab75cb3c91896efdff703094f85727a5dd81c
Pietro <pietros.ifpr2024@gmail.com>
2025-11-09 21:53:00 -0300

    Update execucao.php

--- execucao.php
+++ execucao.php
@@ -5,54 +5,64 @@ require_once("modelo/Prova.php");
 require_once("modelo/Participacao.php");
 require_once("modelo/Trabalho.php");
 
-$instrumento = new Instrumento;
-$instrumento -> setNota(60);
+
 $notas = array();
 
-do{
+do {
 
-echo "-------MENU------- \n";
-echo "1- Ver média final \n";
-echo "2- Fazer prova \n";
-echo "3- Fazer trabalho \n";
-echo "4- Ver nota de participação \n";
-echo "0- Sair \n";
-echo "------------------ \n";
+    echo "\n-------MENU------- \n";
+    echo "1- Ver média final \n";
+    echo "2- Fazer prova \n";
+    echo "3- Fazer trabalho \n";
+    echo "4- Ver nota de participação \n";
+    echo "0- Sair \n";
+    echo "------------------ \n";
 
-$opcao = readline("Escolha uma opção: \n");
+    $opcao = readline("Escolha uma opção: \n");
 
-switch ($opcao) {
-    case 1:
-       
-        echo "Sua média final é";
+    switch ($opcao) {
+        case 1:
 
-        break;
-    case 2:
-        $prova = new Prova;
-        $prova->setNota(readline("\n Informe a sua nota na prova: "));
-        $prova->getNotaFinal();  
-        echo "Sua nota com a prova é: " . $prova->getNotaFinal();
+            $total = 0;
+            $quantidade = count($notas);
 
-        array_push($notas, $prova);
+            if ($quantidade > 0) {
+                foreach ($notas as $notaObj) {
+                    $total += $notaObj->getNotaFinal();
+                }
+                $media = $total / $quantidade;
+            } else {
+                $media = 0;
+            }
 
-      break;
-    case 3:
-        $trabalho = new Trabalho;
-        $trabalho->setNota(readline("\n Informe a sua nota no trabalho: "));
-        $trabalho->getNotaFinal();
-        echo "Sua nota com o trabalho é: " . $trabalho->getNotaFinal();
+            echo "\nSua média final é: " . $media . "\n";
+            break;
+        case 2:
+            $prova = new Prova;
+            $prova->setNota(readline("Informe a sua nota na prova: \n"));
+            $prova->getNotaFinal();
+            echo "Sua nota com a prova é: " . $prova->getNotaFinal() . "\n";
 
-        array_push($notas, $trabalho);
+            array_push($notas, $prova);
 
-        break;
-    case 4:
-        $participacao = new Participacao;
-        $participacao->setNota(readline("\n Informe a sua nota na prova: "));
-        $participacao->getNotaFinal();
-        echo "Sua nota de participação é: " . $participacao->getNotaFinal();
+            break;
+        case 3:
+            $trabalho = new Trabalho;
+            $trabalho->setNota(readline("Informe a sua nota no trabalho: \n"));
+            $trabalho->getNotaFinal();
+            echo "Sua nota com o trabalho é: " . $trabalho->getNotaFinal() . "\n";
 
-        array_push($notas, $participacao);
+            array_push($notas, $trabalho);
 
-        break;
-}
-}while ($opcao != 0);
+            break;
+        case 4:
+            $participacao = new Participacao;
+            $participacao->setNota(readline("\n Informe a sua nota: "));
+            $participacao->getNotaFinal();
+            echo "Sua nota de participação é: " . $participacao->getNotaFinal() . "\n";
+
+            array_push($notas, $participacao);
+
+            break;
+    }
+} while ($opcao != 0);
