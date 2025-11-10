4d895cb234599c0c7196e8b4bb72c4e7277ae383
Pietro <pietros.ifpr2024@gmail.com>
2025-11-09 21:52:18 -0300

    Refactor getNotaFinal method to improve logic

--- modelo/Prova.php
+++ modelo/Prova.php
@@ -5,16 +5,15 @@ require_once("Instrumento.php");
 class Prova extends Instrumento{
     //Métodos
 
-    public function getNotaFinal(){
-        return parent::nota + ($this->nota*0.3);
+    public function getNotaFinal(): float{
+        $notaf =  $this->nota + ($this->nota*0.3);
 
-         if($this->nota > 10){
+         if($notaf > 10){
             return 10;
-        else{
-            return nota;
+         }else{
+            return $notaf;
         }
             
         }
 
     }
-}
